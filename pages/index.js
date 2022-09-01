c49fd23a529819efc41cb35f539a8cb84e6d2926
conradqq <connorhawkes@gmail.com>
2022-09-01 17:07:17 -0400

    Finished, ready for customization

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import About from '../components/About'
+import Contact from '../components/Contact'
+import Main from '../components/Main'
 import Navbar from '../components/Navbar'
+import Projects from '../components/Projects'
+import Skills from '../components/Skills'
 
 
 export default function Home() {
@@ -12,7 +17,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
     <Navbar />
-      
+    <Main />
+    <About /> 
+    <Skills />
+    <Projects />
+    <Contact />
     </div>
   )
 }
